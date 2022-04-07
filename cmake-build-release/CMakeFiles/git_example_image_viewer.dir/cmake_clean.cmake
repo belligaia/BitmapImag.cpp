e171d43eb1188a82fbb69b99d35bc83bdccb0ab3
Gaia Belli <gaiabelli0@gmail.com>
2022-04-07 17:19:08 +0200

    Initial commit

---
+++ cmake-build-release/CMakeFiles/git_example_image_viewer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_example_image_viewer.dir/BitmapImage.cpp.obj"
+  "CMakeFiles/git_example_image_viewer.dir/main.cpp.obj"
+  "git_example_image_viewer.exe"
+  "git_example_image_viewer.exe.manifest"
+  "git_example_image_viewer.pdb"
+  "libgit_example_image_viewer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/git_example_image_viewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
